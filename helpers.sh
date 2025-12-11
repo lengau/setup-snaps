418ad51c56cef53c91cb11b6bfcd8fb3a5b33aba
Alex Lowe <alex.lowe@canonical.com>
2025-12-10 21:46:12 -0500

    feat: more detail in the cache script

--- helpers.sh
+++ helpers.sh
@@ -7,11 +7,14 @@ function cache_snap(){
   mv "old_snaps/${name}_*" . || true
   old_snaps="$(find . -type f -regextype egrep -regex ".*/${name}_[0-9]+\.(snap|assert)")"
   if [[ -z "${old_snaps}" ]]; then
+    echo "Downloading ${snap_line}"
     # shellcheck disable=SC2086
     snap download ${snap_line}  # write the line as-is for the rest of the command.
   else
-    metadata="$(curl --unix-socket /run/snapd.socket http://./v2/find?name="${name}")"
-    echo "$metadata" | jq
+    metadata="$(curl -s --unix-socket /run/snapd.socket http://./v2/find?name="${name}")"
+    if [[ "${RUNNER_DEBUG}" == 1 ]]; then
+      echo "$metadata" | jq
+    fi
     if [[ -z "${channel}" ]]; then
       channel="$(echo "$metadata" | jq -r .result[].channel)"
       if [[ ! "$channel" == */* ]]; then
@@ -21,8 +24,11 @@ function cache_snap(){
     latest_revision=$(echo "$metadata" | jq -r ".result[].channels[\"$channel\"].revision")
     if [[ ! -f "${name}_${latest_revision}.snap" ]]; then
       rm -f "${name}"*
+      echo "Updating ${snap_line}"
       # shellcheck disable=SC2086
       snap download $snap_line  # write the line as-is for the rest of the command.
+    else
+      echo "Already up-to-date: ${snap_line}"
     fi
   fi
 }
